6a8b755305c1b80485e0ca9b6373e5e349d838b8
sakib963 <abdullah15-963@diu.edu.bd>
2025-10-28 08:38:03 +0600

    feat (reports): inventory related reports

--- src/routes/manager/route.js
+++ src/routes/manager/route.js
@@ -14,6 +14,7 @@ const { invoiceRouter } = require("./invoice/route");
 const { productReturnRouter } = require("./product-return/route");
 const { disposeRouter } = require("./product_dispose/route");
 const { managerDashboardRouter } = require("./dashboard/route");
+const { reportsRouter } = require("./reports/route");
 
 const router = Router();
 
@@ -31,7 +32,8 @@ router.use(SUB_CONTEXTS.ATTENDANCE, attendanceRouter);
 router.use(SUB_CONTEXTS.INVOICE, invoiceRouter);
 router.use(SUB_CONTEXTS.PRODUCT_RETURN, productReturnRouter);
 router.use(SUB_CONTEXTS.PRODUCT_DISPOSE, disposeRouter);
-router.use(SUB_CONTEXTS.DASHBOARD, managerDashboardRouter)
+router.use(SUB_CONTEXTS.DASHBOARD, managerDashboardRouter);
+router.use(SUB_CONTEXTS.REPORTS, reportsRouter);
 
 // If you have other routes, you can add them here.
 // Example: adminRouter.use("/other-module", otherModuleRouter);
